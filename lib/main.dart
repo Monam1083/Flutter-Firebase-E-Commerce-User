e2d18afd3190373688e9bb0cd6225ee4cab95b47
Monam1083 <monammajeed1083@gmail.com>
2026-06-28 13:05:10 +0500

    add provider and shared preferences dependices

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.red,
       ),
       home: const HomeScreen(),
     );
