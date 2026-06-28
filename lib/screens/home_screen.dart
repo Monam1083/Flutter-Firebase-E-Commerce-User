a7f99e645125ba8e1dba7336a0322bec893e3872
Monam1083 <monammajeed1083@gmail.com>
2026-06-28 12:42:39 +0500

    design home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Hello world",
+            style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("Hello world")),
+        ],
+      ),
+    );
+  }
+}
