a7ab0b59fc43478220f8bfc15e36b2ea4820c90b
Monam1083 <monammajeed1083@gmail.com>
2026-06-28 14:00:23 +0500

    theme

---
+++ lib/Provider/theme_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  static const THEME_STATUS = "THEME_STATUS";
+  bool _darktheme = false;
+  bool get getisDarktheme => _darktheme;
+
+  Future<void> setDarkTheme(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(THEME_STATUS, value);
+    _darktheme = value;
+    notifyListeners();
+  }
+
+  Future<bool> getTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _darktheme = prefs.getBool(THEME_STATUS) ?? false;
+    notifyListeners();
+    return _darktheme;
+  }
+}
