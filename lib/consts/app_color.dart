f36c9f5ed8944b0de77ffab4f60d7aaed1c77a55
Monam1083 <monammajeed1083@gmail.com>
2026-06-28 13:35:09 +0500

    create colors for app

---
+++ lib/consts/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color lightscafoldcolor = Colors.white;
+  static const Color lightPrimary = Color(0xFF181059);
+  static const Color lightcardcolor = Color.fromARGB(106, 250, 250, 250);
+  static const Color darkscafoldcolor = Color.fromARGB(255, 9, 3, 27);
+  static const Color darkPrimdary = Color.fromARGB(255, 94, 75, 236);
+}
