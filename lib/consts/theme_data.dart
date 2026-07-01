0656773890c353426c046785b9b9ced9c0e497d2
Monam1083 <monammajeed1083@gmail.com>
2026-07-01 15:33:43 +0500

    make theme color for scafold background

--- lib/consts/theme_data.dart
+++ lib/consts/theme_data.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:shop_user_application/consts/app_color.dart';
 
+class Style {
+  static ThemeData themeData({
+    required bool isDarkTheme,
+    required BuildContext context,
+  }) {
+    return ThemeData(
+      scaffoldBackgroundColor: isDarkTheme
+          ? AppColor.darkscafoldcolor
+          : AppColor.lightscafoldcolor,
+      cardColor: isDarkTheme
+          ? const Color.fromARGB(255, 13, 6, 37)
+          : AppColor.lightcardcolor,
+    );
+  }
+}
