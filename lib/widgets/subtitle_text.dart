f96c0fe125db7c8cb863b510d0509c8a67323445
Monam1083 <monammajeed1083@gmail.com>
2026-07-02 12:05:25 +0500

    add text

--- lib/widgets/subtitle_text.dart
+++ lib/widgets/subtitle_text.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class SubtitleTextWidget extends StatelessWidget {
+  const SubtitleTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello world");
+  }
+}
