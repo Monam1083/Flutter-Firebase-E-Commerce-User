f061ff9ed57999dd50f3edb52f8b8f2138df7c43
Monam1083 <monammajeed1083@gmail.com>
2026-07-12 16:52:16 +0500

    add textsyle properties in subtitle text

--- lib/widgets/subtitle_text.dart
+++ lib/widgets/subtitle_text.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
 class SubtitleTextWidget extends StatelessWidget {
-  const SubtitleTextWidget({super.key, required this.label});
+  const SubtitleTextWidget({
+    super.key,
+    required this.label,
+    required this.fontsize,
+    required this.fontStyle,
+    required this.fontWeight,
+    required this.color,
+    required this.textDecoration,
+  });
   final String label;
+  final double fontsize;
+  final FontStyle fontStyle;
+  final FontWeight fontWeight;
+  final Color color;
+  final TextDecoration textDecoration;
+
   @override
   Widget build(BuildContext context) {
     return Text(
       label,
       style: TextStyle(
-        fontSize: 50,
-        fontWeight: FontWeight.bold,
-        color: Colors.redAccent,
+        fontSize: fontsize,
+        fontWeight: fontWeight,
+        color: color,
+        fontStyle: fontStyle,
+        decoration: textDecoration,
       ),
     );
   }
