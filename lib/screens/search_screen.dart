b1f3ef3961bcc0dd39813e183782da2586bc834c
Monam1083 <monammajeed1083@gmail.com>
2026-07-20 19:11:22 +0500

    create search screen option

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shop_user_application/widgets/title_text.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TitleText(
+          label: "search",
+          fontsize: 10,
+          color: Colors.amber,
+          maxlines: 1,
+        ),
+      ),
+    );
+  }
+}
