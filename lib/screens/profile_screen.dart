82b345ef2465035f716af7f3a73452e7016b900d
Monam1083 <monammajeed1083@gmail.com>
2026-07-22 22:06:14 +0500

    add image in app bar of profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -7,6 +7,10 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Image.asset("assets\images\bag\shopping_cart.png"),
+        title: Text("Profile Screen"),
+      ),
       body: Center(
         child: TitleText(
           label: "Profile Screen",
