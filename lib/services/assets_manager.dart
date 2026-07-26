86f220cd28472fc303a3df1b74bd5453bb857c9b
Monam1083 <monammajeed1083@gmail.com>
2026-07-26 17:06:12 +0500

    fix profile image path

--- lib/services/assets_manager.dart
+++ lib/services/assets_manager.dart
@@ -16,8 +16,7 @@ class AssetsManager {
   static String successful = "$imagesPath/successful.png";
 
   // Profile
-  static String address =
-      "shop_user_application\assets\images\profile\address.png";
+  static String address = "$profileImagesPath/address.png";
   static String login = "$profileImagesPath/login.png";
   static String logout = "$profileImagesPath/logout.png";
   static String privacy = "$profileImagesPath/privacy.png";
