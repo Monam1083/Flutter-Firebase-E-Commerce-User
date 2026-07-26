c7fb666aa8a98c3e2bfb5d9e217fac0c6c2d164a
Monam1083 <monammajeed1083@gmail.com>
2026-07-26 17:12:38 +0500

    fix

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,42 +1,44 @@
-import 'dart:ffi';
-import 'dart:ui_web';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
-import 'package:shop_user_application/widgets/subtitle_text.dart';
-import 'package:shop_user_application/widgets/title_text.dart';
+import 'package:provider/provider.dart';
+import 'package:shopsmart_users_en/services/assets_manager.dart';
+import 'package:shopsmart_users_en/widgets/subtitle_text.dart';
+
+import '../providers/theme_provider.dart';
+import '../widgets/title_text.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Image.asset("assets\images\bag\shopping_cart.png"),
+          child: Image.asset(AssetsManager.shoppingCart),
         ),
-        title: Text("Profile Screen"),
+        title: const Text("Profile screen"),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Visibility(
+          const Visibility(
             visible: false,
             child: Padding(
-              padding: EdgeInsetsGeometry.all(10),
-              child: TitleText(
+              padding: EdgeInsets.all(18.0),
+              child: TitlesTextWidget(
                 label: "Please login to have unlimited access",
-                fontsize: 10,
-                color: Colors.black,
-                maxlines: 1,
               ),
             ),
           ),
           Visibility(
             visible: true,
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
               child: Row(
                 children: [
                   Container(
@@ -46,51 +48,89 @@ class ProfileScreen extends StatelessWidget {
                       shape: BoxShape.circle,
                       color: Theme.of(context).cardColor,
                       border: Border.all(
-                        color: Theme.of(context).colorScheme.surface,
+                        color: Theme.of(context).colorScheme.background,
                         width: 3,
                       ),
-                      image: DecorationImage(image: NetworkImage("url")),
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
+                        ),
+                        fit: BoxFit.fill,
+                      ),
                     ),
                   ),
-                  SizedBox(width: 10),
+                  const SizedBox(width: 10),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TitleText(
-                        label: "Monam Majeed",
-                        fontsize: 10,
-                        color: Colors.black,
-                        maxlines: 1,
-                      ),
-                      SizedBox(width: 10),
-                      SubtitleTextWidget(
-                        label: "Monammajeed@gmail.com",
-                        fontsize: 7,
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black,
-                        textDecoration: TextDecoration.none,
-                      ),
+                    children: const [
+                      TitlesTextWidget(label: "Hadi Kachmar"),
+                      SizedBox(height: 6),
+                      SubtitleTextWidget(label: "Coding.with.hadi@gmail.com"),
                     ],
                   ),
                 ],
               ),
             ),
           ),
-          SizedBox(height: 14),
+          const SizedBox(height: 15),
           Padding(
-            padding: EdgeInsetsGeometry.all(8),
+            padding: const EdgeInsets.all(14.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("General"),
-                SizedBox(height: 10),
-                CustomWidget(
-                  imagepath: AssetManager.address,
+                const Divider(thickness: 1),
+                const SizedBox(height: 10),
+                const TitlesTextWidget(label: "General"),
+                const SizedBox(height: 10),
+                CustomListTile(
                   text: "All Order",
+                  imagePath: AssetsManager.orderSvg,
                   function: () {},
                 ),
+                CustomListTile(
+                  text: "Wishlist",
+                  imagePath: AssetsManager.wishlistSvg,
+                  function: () {},
+                ),
+                CustomListTile(
+                  text: "Viewed recently",
+                  imagePath: AssetsManager.recent,
+                  function: () {},
+                ),
+                CustomListTile(
+                  text: "Address",
+                  imagePath: AssetsManager.address,
+                  function: () {},
+                ),
+                const SizedBox(height: 6),
+                const Divider(thickness: 1),
+                const SizedBox(height: 6),
+                const TitlesTextWidget(label: "Settings"),
+                const SizedBox(height: 10),
+                SwitchListTile(
+                  secondary: Image.asset(AssetsManager.theme, height: 34),
+                  title: Text(
+                    themeProvider.getIsDarkTheme ? "Dark Mode" : "Light Mode",
+                  ),
+                  value: themeProvider.getIsDarkTheme,
+                  onChanged: (value) {
+                    themeProvider.setDarkTheme(themeValue: value);
+                  },
+                ),
               ],
+            ),
+          ),
+          Center(
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              onPressed: () {},
+              icon: const Icon(Icons.login),
+              label: const Text("Login"),
             ),
           ),
         ],
@@ -99,14 +139,14 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
-class CustomWidget extends StatelessWidget {
-  const CustomWidget({
+class CustomListTile extends StatelessWidget {
+  const CustomListTile({
     super.key,
-    required this.imagepath,
+    required this.imagePath,
     required this.text,
     required this.function,
   });
-  final String imagepath, text;
+  final String imagePath, text;
   final Function function;
   @override
   Widget build(BuildContext context) {
@@ -114,9 +154,9 @@ class CustomWidget extends StatelessWidget {
       onTap: () {
         function();
       },
-      title: Text(text),
-      leading: Image.asset(imagepath),
-      trailing: Icon(IconlyLight.arrowRight),
+      title: SubtitleTextWidget(label: text),
+      leading: Image.asset(imagePath, height: 34),
+      trailing: const Icon(IconlyLight.arrowRight2),
     );
   }
 }
--- lib/widgets/subtitle_text.dart
+++ lib/widgets/subtitle_text.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
 class SubtitleTextWidget extends StatelessWidget {
   const SubtitleTextWidget({
     super.key,
     required this.label,
-    required this.fontsize,
-    required this.fontStyle,
-    required this.fontWeight,
-    required this.color,
-    required this.textDecoration,
+    this.fontSize = 18,
+    this.fontStyle = FontStyle.normal,
+    this.fontWeight = FontWeight.normal,
+    this.color,
+    this.textDecoration = TextDecoration.none,
   });
-  final String label;
-  final double fontsize;
-  final FontStyle fontStyle;
-  final FontWeight fontWeight;
-  final Color color;
-  final TextDecoration textDecoration;
 
+  final String label;
+  final double fontSize;
+  final FontStyle fontStyle;
+  final FontWeight? fontWeight;
+  final Color? color;
+  final TextDecoration textDecoration;
   @override
   Widget build(BuildContext context) {
     return Text(
       label,
       style: TextStyle(
-        fontSize: fontsize,
+        fontSize: fontSize,
         fontWeight: fontWeight,
+        decoration: textDecoration,
         color: color,
         fontStyle: fontStyle,
-        decoration: textDecoration,
       ),
     );
   }
