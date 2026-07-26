d5af91fef4bde057f4baf7d87d1b9776414f573f
Monam1083 <monammajeed1083@gmail.com>
2026-07-26 17:24:53 +0500

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_user_application/Provider/theme_provider.dart';
 import 'package:shopsmart_users_en/consts/app_colors.dart';
 import 'package:shopsmart_users_en/providers/theme_provider.dart';
 
