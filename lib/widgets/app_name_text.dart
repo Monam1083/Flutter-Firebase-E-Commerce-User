e4fbfa4c7f70513eae29fd9f4a02d73e802d316d
Monam1083 <monammajeed1083@gmail.com>
2026-07-26 21:42:17 +0500

    select colors for app name text with shimmer effect

--- lib/widgets/app_name_text.dart
+++ lib/widgets/app_name_text.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:shop_user_application/widgets/title_text.dart';
 
 class AppNameTextWidget extends StatelessWidget {
   const AppNameTextWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Shimmer.fromColors(
+      baseColor: Colors.purple,
+      highlightColor: Colors.red,
+      child: TitlesTextWidget(label: "Shop Smart"),
+    );
   }
 }
