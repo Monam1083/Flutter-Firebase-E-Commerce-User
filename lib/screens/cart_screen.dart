41ffe88292de650cf9e03595fd6b3914dee35810
Monam1083 <monammajeed1083@gmail.com>
2026-07-27 20:27:17 +0500

    add image

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,10 +1,15 @@
+import 'dart:ui_web';
+
 import 'package:flutter/material.dart';
+import 'package:shop_user_application/services/assets_manager.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column());
+    return Scaffold(
+      body: Column(children: [Image.asset(AssetsManager.shoppingBasket)]),
+    );
   }
 }
