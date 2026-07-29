56f7b13e3443965b3d045ddc5ced19bafae93d2c
Monam1083 <monammajeed1083@gmail.com>
2026-07-29 17:58:44 +0500

    remove the properties

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -8,36 +8,6 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Image.asset(
-              AssetsManager.shoppingBasket,
-              width: double.infinity,
-              height: size.height * 0.35,
-            ),
-            SizedBox(height: 20),
-            TitlesTextWidget(label: "Woops", color: Colors.red, fontSize: 20),
-            SizedBox(height: 20),
-            SubtitleTextWidget(
-              label: "your cart is empty",
-              fontWeight: FontWeight.w600,
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SubtitleTextWidget(
-                label: "looks like your cart is empty add something ",
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: Text("shop Now")),
-          ],
-        ),
-      ),
-    );
+    return Scaffold(body: Column());
   }
 }
