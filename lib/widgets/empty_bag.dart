099126a99d31b83d7c4ea2b80cb539353f4f3bf0
Monam1083 <monammajeed1083@gmail.com>
2026-07-29 18:03:26 +0500

    add size

--- lib/widgets/empty_bag.dart
+++ lib/widgets/empty_bag.dart
@@ -8,6 +8,7 @@ class EmptyBagWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       physics: BouncingScrollPhysics(),
       child: Column(
