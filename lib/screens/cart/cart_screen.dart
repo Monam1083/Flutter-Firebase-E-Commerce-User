63d18974df4602fec479746b062ee2c9269bdd86
Monam1083 <monammajeed1083@gmail.com>
2026-07-29 19:08:39 +0500

    add app bar in other scafold

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop_user_application/screens/cart/cart_widget.dart';
 import 'package:shop_user_application/services/assets_manager.dart';
 import 'package:shop_user_application/widgets/empty_bag.dart';
+import 'package:shop_user_application/widgets/title_text.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -18,6 +19,13 @@ class CartScreen extends StatelessWidget {
             ),
           )
         : Scaffold(
+            appBar: AppBar(
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(AssetsManager.shoppingCart),
+              ),
+              title: TitlesTextWidget(label: "cart Screen"),
+            ),
             body: ListView.builder(
               itemBuilder: (context, index) {
                 return CartWidget();
