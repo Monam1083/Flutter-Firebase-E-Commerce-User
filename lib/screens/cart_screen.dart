e9653ace3eb282784396cedc5d101659d174a129
Monam1083 <monammajeed1083@gmail.com>
2026-07-29 18:35:18 +0500

    access the empty bag properties in cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,9 +1,20 @@
+import 'dart:ui_web';
+
 import 'package:flutter/material.dart';
+import 'package:shop_user_application/services/assets_manager.dart';
+import 'package:shop_user_application/widgets/empty_bag.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column());
+    return Scaffold(
+      body: EmptyBagWidget(
+        imagepath: AssetsManager.shoppingBasket,
+        title: "your cart is empty",
+        subtitle: "Looks like your cart is empty try something to add",
+        buttontext: "shop Now",
+      ),
+    );
   }
 }
--- lib/widgets/empty_bag.dart
+++ lib/widgets/empty_bag.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:shop_user_application/widgets/subtitle_text.dart';
 import 'package:shop_user_application/widgets/title_text.dart';
 
