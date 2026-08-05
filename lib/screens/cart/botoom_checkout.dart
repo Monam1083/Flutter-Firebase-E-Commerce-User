53b9ae45f6eb0bf1ff6d35323be550746219c8d8
Monam1083 <monammajeed1083@gmail.com>
2026-08-05 11:10:23 +0500

    add title fpr products

--- lib/screens/cart/botoom_checkout.dart
+++ lib/screens/cart/botoom_checkout.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shop_user_application/widgets/title_text.dart';
 
 class CartBottomSheetWidget extends StatelessWidget {
   const CartBottomSheetWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-        
-      ],
-    );
+    return Row(children: [TitlesTextWidget(label: "Total")]);
   }
 }
