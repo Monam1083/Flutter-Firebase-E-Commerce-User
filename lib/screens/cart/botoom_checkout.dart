bd32958ce1ee2755b859312ef1cd8bbbeebe7d62
Monam1083 <monammajeed1083@gmail.com>
2026-08-05 11:07:31 +0500

    fix

--- lib/screens/cart/botoom_checkout.dart
+++ lib/screens/cart/botoom_checkout.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class CartBottomSheetWidget extends StatelessWidget {
+  const CartBottomSheetWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
