e1da6f30112e315fef08faab1f7e2307cda233fd
Monam1083 <monammajeed1083@gmail.com>
2026-08-05 11:20:21 +0500

    add button to check out products

--- lib/screens/cart/botoom_checkout.dart
+++ lib/screens/cart/botoom_checkout.dart
@@ -20,6 +20,7 @@ class CartBottomSheetWidget extends StatelessWidget {
                 SubtitleTextWidget(label: "price", color: Colors.blue),
               ],
             ),
+            ElevatedButton(onPressed: onPressed, child: child),
           ],
         ),
       ),
