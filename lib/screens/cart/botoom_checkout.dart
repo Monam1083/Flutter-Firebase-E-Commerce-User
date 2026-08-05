e739dd88e6bd99a0dff31c7762ca4d1dbf913182
Monam1083 <monammajeed1083@gmail.com>
2026-08-05 11:06:48 +0500

    update

--- lib/screens/cart/botoom_checkout.dart
+++ lib/screens/cart/botoom_checkout.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
 
-class BotoomCheckout extends StatelessWidget {
-  const BotoomCheckout({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
