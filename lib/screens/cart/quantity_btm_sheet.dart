2e0e8448d8db818c2f1bf92f9a68f3d1e0ab7a0a
Monam1083 <monammajeed1083@gmail.com>
2026-08-06 17:05:23 +0500

    add subtittle text

--- lib/screens/cart/quantity_btm_sheet.dart
+++ lib/screens/cart/quantity_btm_sheet.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shop_user_application/widgets/subtitle_text.dart';
 
 class QuantityBtmSheetWidget extends StatelessWidget {
   const QuantityBtmSheetWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return SubtitleTextWidget(label: index.toString());
+      },
+    );
   }
 }
