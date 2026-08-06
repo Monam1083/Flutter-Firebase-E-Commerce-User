bc5bcc7f24178ececd44cca54f0de4abfe13a166
Monam1083 <monammajeed1083@gmail.com>
2026-08-06 17:09:32 +0500

    add await in async funtion

--- lib/screens/cart/cart_widget.dart
+++ lib/screens/cart/cart_widget.dart
@@ -62,7 +62,9 @@ class CartWidget extends StatelessWidget {
                         ),
                         const Spacer(),
                         OutlinedButton.icon(
-                          onPressed: () async {},
+                          onPressed: () async {
+                            await 
+                          },
                           icon: const Icon(IconlyLight.arrowDown2),
                           label: const Text("Qty: 6"),
                           style: OutlinedButton.styleFrom(
